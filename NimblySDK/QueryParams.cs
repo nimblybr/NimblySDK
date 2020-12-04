903e9ea81045b0a06f622677092cfaa10626291a
Henrique Netzka <hnetzka@tatico.net>
2020-12-04 11:35:34 -0300

    up do SDK

---
+++ NimblySDK/QueryParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimblySDK
+{
+    public class QueryParams : List<KeyValuePair<string, string>>
+    {
+        public void Add(string key, string value)
+        {
+            this.Add(new KeyValuePair<string, string>(key, value));
+        }
+    }
+}
---
+++ NimblySDKTests/NimblyClientTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using NimblySDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimblySDK.Tests
+{
+    [TestClass()]
+    public class NimblyClientTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            NimblyClient nimblyClient = new NimblyClient()
+            {
+                CN = "Tatico"
+            };
+
+            Console.Write(JsonConvert.SerializeObject(nimblyClient.Get("api/ping")));
+        }
+    }
+}
